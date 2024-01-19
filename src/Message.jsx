f31296d4b0ace0f3d469c467374ca777683a9fa4
Mahardhika Pratama <putrad578@gmail.com>
2024-01-20 01:01:37 +0700

    fix anoyying style

--- src/Message.jsx
+++ src/Message.jsx
@@ -4,7 +4,7 @@ const Message = ({ username, text, date, time }) => {
   return (
     <div className="flex flex-col relative">
       <p className="text-green-600 font-bold break-words text-sm">{username} <span className='text-xs text-gray-400 font-normal'> {date} | {time}</span></p>
-      <p className="font-small break-words mb-6 text-sm">{text}</p>
+      <p className="font-small break-words text-sm">{text}</p>
       <span className="absolute right-0 bottom-0 font-bold text-xs"></span>
     </div>
   );
